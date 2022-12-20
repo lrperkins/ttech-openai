dc536e1b213a1f0125efe533487b9764c3ca7628
Levi Perkins <levi.r.perkins@gmail.com>
2022-12-20 10:46:45 -0500

    feat(main-page)

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { Tooltip } from "react-tippy";
+import 'react-tippy/dist/tippy.css'
+
+// import { RadioGroup } from '@headlessui/react'
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [promptInput, setPromptInput] = useState("");
+  const [temp, setTemp] = useState(0.5);
+  const [respLimit, setRespLimit] = useState(200);
   const [result, setResult] = useState();
+  const [model, setModel] = useState('text-davinci-003');
 
   async function onSubmit(event) {
     event.preventDefault();
-    const response = await fetch("/api/generate", {
+    const response = await fetch("/api/openprompt", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({
+        model: model,
+        prompt: promptInput,
+        temp: ~~temp,
+        respLimit: ~~respLimit,
+      }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    setPromptInput("");
   }
 
   return (
@@ -29,19 +41,41 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>OpenAI Chat GPT</h3>
         <form onSubmit={onSubmit}>
-          <input
+
+          <p>Select your AI model</p>
+          <label for="text-davinci-003">
+            <input type="radio" id="text-davinci-003" name="model" value="text-davinci-003" onClick={() => setModel('text-davinci-003')} defaultChecked />
+            text-davici-003</label><br />
+          <label for="code-davinci-002">
+            <input type="radio" id="code-davinci-002" name="model" value="code-davinci-002" onClick={() => setModel('code-davinci-002')} />
+            code-davinci-002</label>
+          <br />
+          <Tooltip title="Temperature controls the randomness of the model. Lower values will result in more predictable results, while higher values will result in more surprising results.">
+            <label>Temperature: {temp}</label>
+          </Tooltip>
+          <input type="range" min="0" max="1" step="0.1" defaultValue={temp} onChange={(e) => setTemp(e.target.value)} />
+          <br />
+          <Tooltip title="Response length limit is the maximum number of tokens the model will generate.">
+            <label>Response Length Limit: {respLimit}</label>
+          </Tooltip>
+          <input type="range" min="80" max="3000" step="10" defaultValue={respLimit} onChange={(e) => setRespLimit(e.target.value)} /><br />
+          <textarea
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="prompt"
+            placeholder="Enter a prompt"
+            value={promptInput}
+            onChange={(e) => setPromptInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input style={{ marginBottom: 20 }} type="submit" value="Generate" disabled={!promptInput.length} />
         </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
+        {result &&
+          <div style={{ whiteSpace: 'pre-wrap', margin: '0 auto', maxWidth: 800, minWidth: 500, padding: 10, background: '#eee', filter: `invert(${model === 'text-davinci-003' ? 0 : .9})` }}>
+            {result}
+          </div>
+        }
+      </main >
+    </div >
   );
 }
